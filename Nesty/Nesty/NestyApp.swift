42503cbea9233bdb8a81044380f4d3bd99bbb57e
BEOMSU <rafa71802937@gmail.com>
2026-01-02 16:11:17 +0900

    chore: #1 Initialize iOS project with basic app setup
    기본 프로젝트 구조, 빌드 설정, Xcode workspace, 에셋 및 기본 SwiftUI 엔트리포인트를 생성했습니다.
    앱 식별자, 번들, 팀 정보 및 배포 타겟을 설정하여 아이폰 앱의 최소 실행 환경을 구성했으며,
    샘플 ContentView와 앱 진입점을 포함해 추후 개발의 기반을 마련했습니다.

---
+++ Nesty/Nesty/NestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestyApp.swift
+//  Nesty
+//
+//  Created by 심범수 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct NestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
